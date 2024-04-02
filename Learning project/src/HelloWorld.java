35f7c0f1b62803a06ef06a3889b10675d12cf35b
Vlad Bukreev <2redpoll@gmail.com>
2024-04-02 11:13:00 +0500

    initial commit
    Initial commit

---
+++ Learning project/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main (String [] args) {
+        System.out.println("Hello world!");
+
+        int a = 2, b = 3;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
