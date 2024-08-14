984826e054b8bcee3ddd2d0f4f388389e6c44732
Vlad Bukreev <2redpoll@gmail.com>
2024-08-14 18:30:20 +0500

    new classes
    new classes

--- Learning project/src/newClassCreation.java
+++ Learning project/src/newClassCreation.java
@@ -1,2 +1,21 @@
+class Box {
+    double width;
+    double height;
+    double depth;
+}
+
 public class newClassCreation {
+    public static void main(String[] args) {
+        Box mybox = new Box();
+        double vol;
+
+        mybox.width = 10;
+        mybox.height = 20;
+        mybox.depth = 15;
+
+        //box volume
+        vol = mybox.width * mybox.height * mybox.depth;
+        System.out.println("Box volume = " + vol);
+    }
+
 }
